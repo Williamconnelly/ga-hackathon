4b874d311731c1b0331d0bbbde2f7024301c1a08
William Connelly <williamconnelly.dev@gmail.com>
2018-07-20 09:19:55 -0700

    Added demo backend

--- index.js
+++ index.js
@@ -28,6 +28,32 @@ app.get("/api", (req, res) => {
     })
 })
 
+var fakeData = {
+    city: "Seattle",
+    ecycle: "FALSE",
+    geolocation_address: "8105 Fifth Ave. S.",
+    geolocation_zip: "98108",
+    hours: "Thurs-Sat: 9:30am - 4:30pm",
+    location: "8105 Fifth Ave. S. WA 98108",
+    mail_in_allowed: "FALSE",
+    material_handled: "Button Batteries",
+    phone: "(206) 296-4692",
+    pickup_allowed: "FALSE",
+    property_type: "Business, Residents",
+    provider_address: "8105 Fifth Ave. S.",
+    provider_name: "City of Seattle South Hazardous Waste Facility",
+    provider_url: "http://www.lhwmp.org/home/HHW/disposal-locations.aspx#south",
+    providerid: "401",
+    restrictions: "Business waste amounts and types are restricted",
+    service_description: "Accepts button batteries, which are recycled by the Rechargeable Battery Recycling Corporation.",
+    tibn: "FALSE",
+    zip: "98108"
+    }
+
+app.get("/api/fake", (req, res) => {
+    res.render("index", {fakeData})
+})
+
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
